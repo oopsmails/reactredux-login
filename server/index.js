13c9e7521c9525bba9ea1a96b7d2159267ea9c7e
oopsmails <oopsmails@gmail.com>
2016-12-12 21:55:49 -0500

    fixed unexpected token, wrong .babelrc location

--- server/index.js
+++ server/index.js
@@ -3,11 +3,20 @@ import path from 'path';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-import webpackConfig from '../webpack.config.babel';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
 
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const compiler = webpack(webpackConfig);
+// app.use(webpackMiddleware(compiler));
+
+app.use(webpackMiddleware(compiler, {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 const SERVER_PORT = 3080;
 
@@ -15,4 +24,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.listen(SERVER_PORT, () => { console.log('Running on locoalhost:' + SERVER_PORT) } );
+app.listen(SERVER_PORT, () => { console.log('Running on locoalhost:' + SERVER_PORT) });
