11a3f68176f4981486a2ab2e76b6048d265cf725
oopsmails <oopsmails@gmail.com>
2016-12-13 09:33:09 -0500

    signup form 1

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SignupForm from './SignupForm';
 
 class SignupPage extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1>Sign Up</h1>
+       <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SignupForm />
+        </div>
       </div>
     );
   }
