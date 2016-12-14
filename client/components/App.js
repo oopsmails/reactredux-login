f0fa499050cf6fd6367ec9ca5efec3d4519677de
oopsmails <oopsmails@gmail.com>
2016-12-13 19:00:07 -0500

    Render flash msg and allow delete it

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Greetings from './Greetings';
 import NavigationBar from "./NavigationBar";
+import FlashMessagesList from './flash/FlashMessagesList';
 
 class App extends React.Component {
     render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
             //<Greetings />
             <div className = "container">
                 <NavigationBar />
+                <FlashMessagesList />
                 { this.props.children }
             </div>
         );
