28d6e88a335417bcd5aebe1e6a005a53fb1a3018
oopsmails <oopsmails@gmail.com>
2016-12-21 19:25:16 -0500

    login and logout on client

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import {logout} from '../actions/authActions';
+
+class NavigationBar extends React.Component {
+    logout(e) {
+        e.preventDefault();
+        this.props.logout();
+    }
 
 
-// class NavigationBar extends React.Component {
-//     render() {
-export default () => {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link to="/" className="navbar-brand">Red Dice</Link>
+    render() {
+// export default () => {
+
+        const {isAuthenticated} = this.props.auth;
+
+        const userLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/signup">Sign up</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        );
+
+
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">Red Dice</Link>
+                    </div>
+
+                    {/*<div className="collapse navbar-collapse">*/}
+                    {/*<ul className="nav navbar-nav navbar-right">*/}
+                    {/*<li><Link to="/signup" className="navbar-brand">Sign up</Link></li>*/}
+                    {/*<li><Link to="/login">Login</Link></li>*/}
+                    {/*</ul>*/}
+                    {/*</div>*/}
+
+                    <div>
+                        { isAuthenticated ? userLinks : guestLinks }
+                    </div>
+
                 </div>
-
-                {/*<div className="collapse navbar-collapse">*/}
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link to="/signup" className="navbar-brand">Sign up</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                    </ul>
-                {/*</div>*/}
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
-// }
 
 
-// export default NavigationBar;
+NavigationBar.propTypes = {
+    auth: React.PropTypes.object.isRequired,
+    logout: React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
